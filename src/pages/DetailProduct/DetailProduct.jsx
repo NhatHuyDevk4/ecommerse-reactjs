1cbc9e5c376f273e27eb8052389984e2870ff236
Le-Nhat-Huy-Qe180061 <lenhathuy9a6@gmail.com>
2024-12-29 23:28:44 +0700

    Done detail product right

--- src/pages/DetailProduct/DetailProduct.jsx
+++ src/pages/DetailProduct/DetailProduct.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
-
+import MyHeader from '@components/Header/Header';
+import MainLayout from '@components/Layout/Layout';
+import React from 'react';
+import style from './styles.module.scss';
 const DetailProduct = () => {
-  return (
-    <div>DetailProduct</div>
-  )
-}
+    const { container, navigateSection, contentSection, imgeBox } = style;
 
-export default DetailProduct
+    return (
+        <div>
+            <MyHeader />
+            <div className={container}>
+                <MainLayout>
+                    <div className={navigateSection}>
+                        <div>Home {'>'} Men</div>
+                        <div style={{ cursor: 'pointer' }}>
+                            {'<'} Return to previous page
+                        </div>
+                    </div>
+
+                    <div className={contentSection}>
+                        <div className={imgeBox}>
+                            <img
+                                src='https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-7.3-min.jpg'
+                                alt='ten'
+                            />
+                            <img
+                                src='https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-7.3-min.jpg'
+                                alt='ten'
+                            />
+                            <img
+                                src='https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-7.3-min.jpg'
+                                alt='ten'
+                            />
+                            <img
+                                src='https://xstore.b-cdn.net/elementor2/marseille04/wp-content/uploads/sites/2/2022/12/Image-7.3-min.jpg'
+                                alt='ten'
+                            />
+                        </div>
+                        <div>left</div>
+                    </div>
+                </MainLayout>
+            </div>
+        </div>
+    );
+};
+
+export default DetailProduct;
