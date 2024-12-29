518b33b0a6072919e3e18bee2ef1c79c5efe3c0a
Le-Nhat-Huy-Qe180061 <lenhathuy9a6@gmail.com>
2024-12-30 00:25:07 +0700

    Detail product right done 50%

--- src/pages/DetailProduct/DetailProduct.jsx
+++ src/pages/DetailProduct/DetailProduct.jsx
@@ -2,8 +2,26 @@ import MyHeader from '@components/Header/Header';
 import MainLayout from '@components/Layout/Layout';
 import React from 'react';
 import style from './styles.module.scss';
+import Button from '@components/Button/Button';
 const DetailProduct = () => {
-    const { container, navigateSection, contentSection, imgeBox } = style;
+    const {
+        container,
+        navigateSection,
+        contentSection,
+        imgeBox,
+        infoBox,
+        price,
+        description,
+        boxSize,
+        size,
+        titleSize,
+        functionInfo,
+        incremenAmout,
+        boxBtn,
+        orSection,
+        line,
+        orText,
+    } = style;
 
     return (
         <div>
@@ -36,7 +54,44 @@ const DetailProduct = () => {
                                 alt='ten'
                             />
                         </div>
-                        <div>left</div>
+                        <div className={infoBox}>
+                            <h1>Title Product</h1>
+                            <p className={price}>$1,879.99</p>
+                            <p className={description}>
+                                Amet, elit tellus, nisi odio velit ut. Euismod
+                                sit arcu, quisque arcu purus orci leo.
+                            </p>
+
+                            <div className={titleSize}>Size</div>
+
+                            <div className={boxSize}>
+                                <div className={size}>L</div>
+                                <div className={size}>M</div>
+                                <div className={size}>S</div>
+                            </div>
+
+                            <div className={functionInfo}>
+                                <div className={incremenAmout}>
+                                    <div>-</div>
+                                    <div>1</div>
+                                    <div>+</div>
+                                </div>
+
+                                <div className={boxBtn} >
+                                    <Button content={'Add to cart'} />
+                                </div>
+                            </div>
+
+                            <div className={orSection}>
+                                <div className={line}/>
+                                <div className={orText}>OR</div>
+                                <div className={line}/>
+                            </div>
+
+                            <div className={boxBtn}>
+                                <Button content={'Buy now'} isPriamry={true}/>
+                            </div>
+                        </div>
                     </div>
                 </MainLayout>
             </div>
